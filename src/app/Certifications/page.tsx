c28a2eadcdc3a69d7aa7e2f05c5302ad7b09da03
Asim74 <asimr6573@gmail.com>
2024-11-03 21:31:29 +0500

    first commit

---
+++ src/app/Certifications/page.tsx
@@ -0,0 +1,14 @@
+
+
+// pages/contact.js
+export default function Contact() {
+    return (
+      <div className="p-6 min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold mb-2">Contact Information</h1>
+      <p className="text-lg mb-4">
+        This is the contact information page.
+      </p>
+         </div>
+    );
+  }
+  
